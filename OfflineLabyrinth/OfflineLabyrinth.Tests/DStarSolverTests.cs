e71da775a6fc0b4c1aa1f8b6457e569ca6b2b771
GhostTyper <matthias@lukaseder.de>
2025-08-10 18:50:08 +0200

    Add timing output to tests

--- OfflineLabyrinth/OfflineLabyrinth.Tests/DStarSolverTests.cs
+++ OfflineLabyrinth/OfflineLabyrinth.Tests/DStarSolverTests.cs
@@ -1,18 +1,30 @@
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace OfflineLabyrinth.Tests
 {
     public class DStarSolverTests
     {
+        private readonly ITestOutputHelper output;
+
+        public DStarSolverTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public async Task DStarSolverSolvesLabyrinth()
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             using (DStarSolver solver = new DStarSolver())
             {
                 string finalLine = await solver.SolveAsync(32, 32, 1);
                 Assert.StartsWith("8 Congratulation.", finalLine);
             }
+            stopwatch.Stop();
+            this.output.WriteLine("DStarSolverSolvesLabyrinth took " + stopwatch.ElapsedMilliseconds + "ms");
         }
     }
 }
